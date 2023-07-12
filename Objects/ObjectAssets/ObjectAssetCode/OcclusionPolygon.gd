2632f86bf23ba8df959ec5eb9ee6e5204674b1df
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-07-12 19:28:05 +0500

    NEW PROJECT LETS GOOO
    - actually, a renewal project
    - but yea
    - lets gooooo
    - also did some testing stuff

---
+++ Objects/ObjectAssets/ObjectAssetCode/OcclusionPolygon.gd
@@ -0,0 +1,22 @@
+extends Polygon2D
+class_name OcclusionPolygon
+
+
+func _ready():
+	var static_body = StaticBody2D.new()
+	add_child(static_body)
+	
+	var collision_pol = CollisionPolygon2D.new()
+	collision_pol.polygon = polygon
+	static_body.add_child(collision_pol)
+	
+	var light_occluder = LightOccluder2D.new()
+	light_occluder.occluder = OccluderPolygon2D.new()
+	light_occluder.occluder.polygon = polygon
+	add_child(light_occluder)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
