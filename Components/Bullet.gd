641c096e66992b1707ec919118066042eca62b3b
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-07-17 23:10:37 +0500

    better gun and cursor

--- Components/Bullet.gd
+++ Components/Bullet.gd
@@ -4,14 +4,23 @@ class_name Bullet
 var speed = 1000
 var damage = 1
 
-
+var visibility_notif: VisibleOnScreenNotifier2D = VisibleOnScreenNotifier2D.new()
+var vis_timer = 5
 func _ready():
+	add_child(visibility_notif)
 	print(damage)
 
 func _physics_process(delta):
+	vis_timer = max(vis_timer-delta , 0)
 	position += Vector2.RIGHT.rotated(rotation)*speed*delta
 	for body in get_overlapping_bodies():
 		if body.is_in_group("destructable"):
 			body.queue_free()
 			queue_free()
+	
+	if visibility_notif.is_on_screen() or vis_timer:
+		return
+	queue_free()
+
+
 
--- Components/Shooter.gd
+++ Components/Shooter.gd
@@ -7,17 +7,17 @@ class_name Shooter
 @export var bullet_type : PackedScene
 @export var bullet_speed: int = 1000
 
-@onready var gun = Sprite2D.new()
+@onready var gun = $Triangle
 
 @onready var parent = get_parent()
 
 
 func _ready():
-	
-	gun.texture = preload("res://icon.svg")
-	gun.apply_scale(Vector2.ONE/5)
-	gun.self_modulate = Color.BLACK
-	gun.offset = Vector2.RIGHT*64
+#
+#	gun.texture = preload("res://icon.svg")
+#	gun.apply_scale(Vector2.ONE/5)
+#	gun.self_modulate = Color.BLACK
+	gun.offset = Vector2.RIGHT*92
 	
 	add_child(gun)
 
@@ -25,6 +25,7 @@ func _ready():
 
 func aim(direction: Vector2):
 	gun.look_at(direction)
+	
 
 func shoot():
 	if parent.charge <= 0:
@@ -32,7 +33,7 @@ func shoot():
 	var bullet : Bullet = bullet_type.instantiate()
 	bullet.rotate(gun.rotation)
 	bullet.speed = 1000
-	bullet.position = global_position + Vector2.RIGHT.rotated(gun.rotation)*24
+	bullet.position = global_position + Vector2.RIGHT.rotated(gun.rotation)*32
 	bullet.modulate = parent.modulate
 	bullet.damage = get_bullet_damage(parent.charge)
 	bullet.z_index = 1
